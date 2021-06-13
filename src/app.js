7e50b0eb0218ec08c2c11f56ec3ce3323c52a867
Sabina Abdulaliieva <77077884+Sabinka1207@users.noreply.github.com>
2021-06-13 23:19:29 +0300

    HW 8 main part done

--- src/app.js
+++ src/app.js
@@ -63,3 +63,41 @@ const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const gallery = document.querySelector(".js-gallery")
+const modal = document.querySelector(".js-lightbox")
+const img = document.querySelector(".lightbox__image")
+const btn = document.querySelector('[data-action="close-lightbox"]')
+// console.log(img)
+
+const items = []
+
+galleryItems.forEach(item => {
+const prev = item.preview
+const origin = item.original
+const descr = item.description
+  item = `
+<li class="gallery__item">
+<a href="#" class="gallery__link">
+<img src="${prev}" data-source="${origin}" class"gallery__image" alt="${descr}"></a>
+</li>`
+  
+  items.push(item)
+});
+
+gallery.insertAdjacentHTML('afterbegin', items.join(" "))
+gallery.addEventListener("click", onImgClick);
+btn.addEventListener("click", onCloseModal)
+
+function onImgClick(event) { 
+  const targetImg = event.target;
+  console.log("event target: ", targetImg)
+  modal.classList.toggle("is-open")
+  img.src = targetImg.dataset.source
+};
+
+function onCloseModal(event) {
+  const targetCloseBtn = event.target;
+  modal.classList.toggle("is-open")
+    img.src = ""
+}
